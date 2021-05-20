6f022a25af2d3324c9bed8195dd94b6ea4a073f3
Emma <emmandersson166@gmail.com>
2021-05-20 13:02:17 +0200

    added and changed datatype

--- api/models/Movie.js
+++ api/models/Movie.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const movieSchema = new Schema({
 title: {type:String},
 description: {type: String},
-genre: {type:String},
+genre: {type:[String]},
 year: {type: String},
 language: {type: String},
 director: {type: String},
@@ -14,6 +14,7 @@ price: {type: Number},
 trailerUrl: {type: String},
 coverImage: {type: String},
 timeLength: {type: String},
+age: {type: String},
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
