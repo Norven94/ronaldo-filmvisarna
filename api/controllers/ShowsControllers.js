ca7b294b48be39fd1edc90091cae22c63b0f5b5a
Oskar Anderberg <otto.anderberg@gmail.com>
2021-05-20 13:51:39 +0200

    continue with shows controller to create shows

--- api/controllers/ShowsControllers.js
+++ api/controllers/ShowsControllers.js
@@ -1,16 +1,48 @@
-const Shows = require("../models/Shows");
+const Show = require("../models/Show");
+const Movie = require("../models/Movie");
+const Salon = require("../models/Salon");
+
+const getDateArray = (start, end) => {
+  let arr = new Array();
+  let dt = new Date(start);
+  while (dt <= end) {
+      arr.push(new Date(dt));
+      dt.setDate(dt.getDate() + 1);
+  }
+  return arr;
+}
 
 const createShows = async (req, res) => {
-    let show = await Shows.create({
+    let movies = await Movie.find().exec();
+    let salons = await Salon.find().exec();
+    const times = ["10:00-14:00", "14:00-18:00", "18:00-22:00"]
+    
+    let startDate = new Date("2021-05-01");
+    let endDate = new Date("2021-05-31");
+    let dates = getDateArray(startDate, endDate)
+
+    dates.map(date => {
+      salons.map(salon => {        
+        times.map(time => {
+          movies.map(movie => {
+
+          })
+        })
+      })
+    })
+
+    /*
+    let show = await Show.create({
         salonId: req.body.salonId,
         movieId: req.body.movieId,
         time: req.body.time,
         date: req.body.date
       });
       console.log("New show: ", show);
-      res.send("Ok again");
+    */
+    res.send("Ok");
 }
 
 module.exports = {
-    createShows
+    createShows,
   };
