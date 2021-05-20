714234a63046ccc4c719cf62443a9041b1ffcbb5
Emma <emmandersson166@gmail.com>
2021-05-20 12:55:12 +0200

    added Movie model

---
+++ api/models/Movie.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const movieSchema = new Schema({
+title: {type:String},
+description: {type: String},
+genre: {type:String},
+year: {type: String},
+language: {type: String},
+director: {type: String},
+artists: {type:[String]},
+price: {type: Number},
+trailerUrl: {type: String},
+coverImage: {type: String},
+timeLength: {type: String},
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
