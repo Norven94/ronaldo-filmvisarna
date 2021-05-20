667fdd718c58436a4e36e1e2daf7c782153300b8
Mehmet Ziya <mehmetziya82@gmail.com>
2021-05-20 15:26:31 +0200

    created AboutPage

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,22 @@
+
+
+
+
+const AboutPage = () => {
+    return ( 
+        <div>
+            <h1>Filmvisarna AB</h1>
+            <h3>About Us</h3>
+            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It hasroots in a piece of classical Latinliterature
+                from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney Collegein Virginia, 
+                looked up one of the more obscure Latin words, consectetur. Contrary to popular belief, Lorem Ipsum is not simply random text. 
+                It has roots in a piece of classical Latinliterature from 45 BC, making it over 2000 years old. Richard McClintock,
+                a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, 
+                consectetur. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
+                Latinliterature from 45 BC, making it over 2000.
+            </p>
+        </div>
+     );
+}
+ 
+export default AboutPage;
