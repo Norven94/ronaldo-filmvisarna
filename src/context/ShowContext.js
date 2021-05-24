442cf4fad372f3d3aacaa499a163f8cba75e2e59
Emma <emmandersson166@gmail.com>
2021-05-24 09:23:22 +0200

    set up in movie details page

--- src/context/ShowContext.js
+++ src/context/ShowContext.js
@@ -4,15 +4,26 @@ export const ShowContext = createContext();
 
 const ShowProvider = (props) => {
   const [currentShows, setCurrentShows] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getAllShowsByMovieId();
+ 
+   
+}, []);
 
   const getAllShowsByMovieId = async (movieId) => {
     let shows = await fetch(`api/v1/shows/${movieId}`);
     shows = await shows.json();
     setCurrentShows(shows);
+    setLoading(false);
+    console.log(currentShows);
   };
 
   const values = {
     getAllShowsByMovieId,
+    currentShows,
+    loading
   };
 
   return (
--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,5 +1,26 @@
-const MovieDetailPage = () => {
-  return <div>MovieDetails</div>;
+import { useContext } from "react";
+
+/* import { MovieContext } from "../context/MovieContext"; */
+import { ShowContext } from "../context/ShowContext";
+
+const MovieDetailPage = (props) => {
+  /*   const { movies } = useContext(MovieContext); */
+  const { currentShows, loading } = useContext(ShowContext);
+
+  const { movieId } = props.match.params;
+
+  /*  const renderMovieInfo = () => {};
+   */
+
+  const renderShowsInfo = () => {
+  };
+
+  return (
+    <div>
+      {loading && <div>LOADING</div>}
+      {currentShows && renderShowsInfo()}
+    </div>
+  );
 };
 
 export default MovieDetailPage;
