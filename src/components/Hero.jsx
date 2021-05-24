31e6b645a4848826b061ac03b2fdcb05bb92562d
Cedrik <cedrikvonheiroth@gmail.com>
2021-05-24 16:36:05 +0200

    added react-bootstrap carousel
    not done yet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,51 @@
 import "../scss/Hero.scss";
+import Carousel from 'react-bootstrap/Carousel';
+
+import theDeparted from "../assets/hero-images/the-departed.jpg";
+import theIntouchables from "../assets/hero-images/the-intouchables.jpg";
+import theMatrix from "../assets/hero-images/the-matrix.jpg";
 
 const Hero = () => {
 
     return(
         <div className="hero">
+            <Carousel className="carouselWrapper" >
+                <Carousel.Item className="carouselItem" >
+                    <img
+                    className="carouselImage"
+                    src={theDeparted}
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>The Departed</h3>
 
-            <div className="heroContent">
-                <h2>Movie title</h2>
-                <span>Genre / </span>
-                <span>Time / </span>
-                <span>Price </span>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="carouselImage"
+                    src={theIntouchables}
+                    alt="Second slide"
+                    />
 
+                    <Carousel.Caption>
+                    <h3>The Intouchables</h3>
 
-                <div className="showsAndTickets">
-                    <p>Shows...</p>
-                    <p>Tickets</p>
-                </div>
-            </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="carouselImage"
+                    src={theMatrix}
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>The Matrix</h3>
+
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import './scss/variables.scss';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
