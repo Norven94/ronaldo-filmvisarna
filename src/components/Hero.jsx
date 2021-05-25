348c4b63ec2184601ed0c6d28cc2f06471d90e1d
Cedrik <cedrikvonheiroth@gmail.com>
2021-05-25 13:59:56 +0200

    Carousel images and info now dynamic from database

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,67 +1,32 @@
 import "../scss/Hero.scss";
 import Carousel from 'react-bootstrap/Carousel';
 
-import theDeparted from "../assets/hero-images/the-departed.jpg";
-import theIntouchables from "../assets/hero-images/the-intouchables.jpg";
-import theMatrix from "../assets/hero-images/the-matrix.jpg";
-
 const Hero = (props) => {
     // receive props from parent Home component for movie data
-    
-    let theDepartedData = props.data[6];
-    let theIntouchablesData = props.data[17];
-    let theMatrixData = props.data[1];
 
-    
+    // creating an array with the popular movies
+    let movies = props.data;
+    let popularMovies = "";
+    if (movies) {popularMovies = movies.filter((movie) => movie.popular === true);}
+
+    // console.log(popularMovies);
+
     return(
         <div className="hero">
-            <Carousel className="carouselWrapper" >
-                <Carousel.Item className="carouselItem" >
-                    <img
-                    className="carouselImage"
-                    src={theDeparted}
-                    alt="First slide"
-                    />
-                    <Carousel.Caption className="carouselCaption">
-                        <h2> {theDepartedData.title} </h2>
-                        <span> {theDepartedData.genre[0]},  </span>
-                        <span> {theDepartedData.genre[1]},  </span>
-                        <span> {theDepartedData.genre[2]} / </span>
-                        <span> {theDepartedData.timeLength} min / </span>
-                        <span> {theDepartedData.price} kr </span>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                
-                <Carousel.Item className="carouselItem">
-                    <img
-                    className="carouselImage"
-                    src={theIntouchables}
-                    alt="Second slide"
-                    />
-                    <Carousel.Caption className="carouselCaption">
-                        <h2> {theIntouchablesData.title} </h2>
-                        <span> {theIntouchablesData.genre[0]}, </span>
-                        <span> {theIntouchablesData.genre[1]}, </span>
-                        <span> {theIntouchablesData.genre[2]} / </span>
-                        <span> {theIntouchablesData.timeLength} min / </span>
-                        <span> {theIntouchablesData.price} kr </span>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-                <Carousel.Item className="carouselItem">
-                    <img
-                    className="carouselImage"
-                    src={theMatrix}
-                    alt="Third slide"
-                    />
-                    <Carousel.Caption className="carouselCaption">
-                        <h2> {theMatrixData.title} </h2>
-                        <span> {theMatrixData.genre[0]}, </span>
-                        <span> {theMatrixData.genre[1]} / </span>
-                        <span> {theMatrixData.timeLength} min / </span>
-                        <span> {theMatrixData.price} kr </span>
-                    </Carousel.Caption>
-                </Carousel.Item>
+            <Carousel className="carouselWrapper">
+                {popularMovies.map((popularMovie) => {
+                    return (
+                        <Carousel.Item className="carouselItem">
+                                <img className="carouselImage" src={popularMovie.heroImage} alt={popularMovie.title} />
+                            <Carousel.Caption className="carouselCaption">
+                                <h2> {popularMovie.title} </h2>
+                                <span> {popularMovie.genre} /</span>
+                                <span> {popularMovie.timeLength} min /</span>
+                                <span> {popularMovie.price} kr</span>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    );
+                })}
             </Carousel>
         </div>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,13 +8,6 @@ import "../scss/HomePage.scss";
 const HomePage = () => {
     const { movies } = useContext(MovieContext);
 
-    let popularMovies = ""
-    // creating an array with the popular movies
-    if (movies) {popularMovies = movies.filter((movie) => movie.popular === true);}
-
-    console.log(movies);
-    console.log("popularMovies: ", popularMovies);
-
     return (
         <div className="homePage">
             {/* send down data to Hero with props */}
