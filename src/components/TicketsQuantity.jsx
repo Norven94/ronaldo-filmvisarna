0e956edf139b15eaf0f14d9cd1d06983e5f5cadd
Emma <emmandersson166@gmail.com>
2021-05-26 09:42:29 +0200

    imported totalTickets and totalSum from BookingContext.js into TicketsQuantity

--- src/components/TicketsQuantity.jsx
+++ src/components/TicketsQuantity.jsx
@@ -1,6 +1,14 @@
+import { useContext } from "react";
+
+import { BookingContext } from "../context/BookingContext";
+
 import "../scss/TicketsQuantity.scss";
 
 const TicketsQuantity = () => {
+  const { totalTickets, setTotalTickets, totalSum, setTotalSum } = useContext(
+    BookingContext
+  );
+
   return <div>Tickets Quantity</div>;
 };
 
