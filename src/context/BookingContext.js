03cc98c7a33870d3bb1f28287d79701a877a67a2
Emma <emmandersson166@gmail.com>
2021-05-27 09:53:33 +0200

    created addBookingToUser function in booking context

--- src/context/BookingContext.js
+++ src/context/BookingContext.js
@@ -3,73 +3,89 @@ import { createContext, useState, useEffect } from "react";
 export const BookingContext = createContext();
 
 const BookingProvider = (props) => {
-    const [seatingMap, setSeatingMap] = useState([]);
-    const [bookings, setBookings]= useState([]);
-    const [booked, setBooked] = useState([{ row: 2, seatNumber: 9 }, { row: 4, seatNumber: 24 }]);
-    const [selected, setSelected] = useState([]);
+  const [seatingMap, setSeatingMap] = useState([]);
+  const [bookings, setBookings] = useState([]);
+  const [booked, setBooked] = useState([
+    { row: 2, seatNumber: 9 },
+    { row: 4, seatNumber: 24 },
+  ]);
+  const [selected, setSelected] = useState([]);
 
-    useEffect(() => {
-        getMyBookings()
+  useEffect(() => {
+    getMyBookings();
+  });
+
+  const getMyBookings = async () => {
+    let data = await fetch("/api/v1/users/bookings");
+    data = await data.json();
+    setBookings(data);
+  };
+
+  const deleteBooking = async (bookingId) => {
+    let result = await fetch(`/api/v1/users/bookings/${bookingId}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(),
     });
+    result = result.json();
+    return result;
+  };
 
-    const getMyBookings = async () => {
-        let data = await fetch("/api/v1/users/bookings");
-        data = await data.json();
-        setBookings(data);
+  const makeSeatingMap = async (salonId) => {
+    let rows = await fetch(`/api/v1/salons/${salonId}`);
+    rows = await rows.json();
+
+    let seatingMap = [];
+    let currentSeatnumber = 1;
+
+    for (let i = 0; i < rows.length; i++) {
+      let row = [];
+
+      // make a loop for every row create a seat object
+      for (let k = 0; k < rows[i]; k++) {
+        let seat = {
+          row: i + 1,
+          seatNumber: currentSeatnumber,
+        };
+        currentSeatnumber = currentSeatnumber + 1;
+        row.push(seat);
+      }
+      seatingMap.push(row);
     }
+    setSeatingMap(seatingMap);
+  };
 
-    const deleteBooking = async (bookingId) => {
-        let result = await fetch(`/api/v1/users/bookings/${bookingId}`, {
-            method: "DELETE",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify()
-        });
-        result = result.json();
-        return result;
-    }
+  const addBookingToUser = async (newShowId) => {
+    let result = await fetch(`/api/v1/users/add/${currentUser.id}`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newShowId),
+    });
+    result = await result.json();
+    getMyBookings();
+    return result;
+  };
 
-    const makeSeatingMap = async (salonId) => {        
-        let rows = await fetch(`/api/v1/salons/${salonId}`);
-        rows = await rows.json();
+  const values = {
+    bookings,
+    deleteBooking,
+    seatingMap,
+    makeSeatingMap,
+    selected,
+    setSelected,
+    booked,
+    addBookingToUser,
+  };
 
-        let seatingMap = [];
-        let currentSeatnumber = 1;
+  return (
+    <BookingContext.Provider value={values}>
+      {props.children}
+    </BookingContext.Provider>
+  );
+};
 
-        for (let i = 0; i < rows.length; i++) {
-            let row = []
-
-            // make a loop for every row create a seat object
-            for (let k = 0; k < rows[i]; k++) {
-                let seat = {
-                    row: i + 1,
-                    seatNumber: currentSeatnumber
-                }
-                currentSeatnumber = currentSeatnumber + 1
-                row.push(seat)
-            }
-            seatingMap.push(row)
-        }
-        setSeatingMap(seatingMap);
-    };
-
-    const values = {
-        bookings,
-        deleteBooking,
-        seatingMap,
-        makeSeatingMap,
-        selected, 
-        setSelected,
-        booked
-    }
-
-    return (
-        <BookingContext.Provider value={values}>
-            {props.children}
-        </BookingContext.Provider>
-    );
-
-}
-
-export default BookingProvider ;
+export default BookingProvider;
