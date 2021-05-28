1e943da491b5422839c520be2b861175e1e6265c
Emma <emmandersson166@gmail.com>
2021-05-28 15:35:33 +0200

    created route/controller getShowById

--- api/routes/ShowsRoutes.js
+++ api/routes/ShowsRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const showsController = require("../controllers/ShowsControllers");
 
-router.post("", showsController.createShows);
+router.get("/show/:id", showsController.getShowById);
 router.get("/:id", showsController.getShowsByMovieId);
+router.post("", showsController.createShows);
+
+
 
 module.exports = router;
