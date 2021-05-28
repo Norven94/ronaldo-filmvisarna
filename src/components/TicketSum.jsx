88267f5457fdc1a1e3ce6412f9b8d3961b183901
Emma <emmandersson166@gmail.com>
2021-05-28 15:37:24 +0200

    split up of components

---
+++ src/components/TicketSum.jsx
@@ -0,0 +1,52 @@
+import { useContext } from "react";
+
+import { BookingContext } from "../context/BookingContext";
+
+import "../scss/TicketsQuantity.scss";
+
+const TicketSum = (props) => {
+  const {
+    price,
+    totalTickets,
+    setTotalTickets,
+    totalSum,
+    setTotalSum,
+    selected,
+  } = useContext(BookingContext);
+
+  const calculateTicketType = (type) => {
+    if (type.ticketType === "Senior") {
+      return type.quantity * price * 0.8;
+    }
+    if (type.ticketType === "Children") {
+      return Math.round(type.quantity * price * 0.7);
+    } else {
+      return type.quantity * price;
+    }
+  };
+
+  return (
+    <div className="ticketSum">
+      {totalTickets.map((type, index) => (
+        <div key={index}>
+          {type.quantity > 0 ? (
+            <div className="flex ticketsOverview">
+              <div className="quantity">
+                <p>{type.quantity}</p>
+                <p>{type.ticketType}</p>
+              </div>
+              <div>
+                <p>{calculateTicketType(type)},-</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      ))}
+      <div className="flex total">
+        <p>Total:</p> <p>{props.totalSum},-</p>
+      </div>
+    </div>
+  );
+};
+
+export default TicketSum;
