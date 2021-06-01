db1a748e89c3d89ddf5e6c1cf9ef3e01ece351c1
Oskar Anderberg <otto.anderberg@gmail.com>
2021-06-01 13:36:27 +0200

    rotate seats and salon

--- src/components/Salon.jsx
+++ src/components/Salon.jsx
@@ -83,7 +83,7 @@ export default function Salon(props) {
                         })}
                     </div>
                 );
-            })}
+            }).reverse()}
         </div>
     );
 }
